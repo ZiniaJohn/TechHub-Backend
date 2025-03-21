80be9ef3924222546f2d7f5f81719b717578d924
ZiniaJohn <ziniajohn123@gmail.com>
2025-03-21 22:20:33 +0500

    hey

--- Techhub-Backend/server.js
+++ Techhub-Backend/server.js
@@ -4,7 +4,7 @@ import { connectDB } from "./config/connectDb.js";
 import cloudinary from "cloudinary";
 import Stripe from "stripe";
 
-// Uncaught Exception Handling
+// Handle Uncaught Exceptions
 process.on("uncaughtException", (error) => {
   console.error(`Uncaught Exception: ${error.message}`);
   process.exit(1);
@@ -15,34 +15,32 @@ config({
   path: "./config/config.env",
 });
 
-// Database connection
+// Connect to Database
 connectDB();
 
-// Stripe configuration
+// Stripe Configuration
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-// Cloudinary configuration
+// Cloudinary Configuration
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
+// Start Server
 const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => {
+  console.log(`✅ Server is running on PORT: ${PORT}`);
+});
 
-let server;
-if (!server) {
-  server = app.listen(PORT, () => {
-    console.log(`✅ Server is running on PORT: ${PORT}`);
-  });
-
-  server.timeout = 300000;
-}
-
-// Unhandled Promise Rejections
+// Handle Unhandled Promise Rejections
 process.on("unhandledRejection", (error) => {
   console.error(`Unhandled Promise Rejection: ${error.message}`);
   server.close(() => {
     process.exit(1);
   });
 });
+
+// **Export for Vercel**
+export default app;
